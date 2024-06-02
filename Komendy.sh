cee0b861c36601788e2011e7755b88597aad92a3
Krystian <krystian-galus@o2.pl>
2024-06-02 18:31:37 +0200

    Pierwszy commit

---
+++ Komendy.sh
@@ -0,0 +1,17 @@
+git commit
+git checkout -b 
+git merge 
+git rebase main
+git checkout nazwa
+git checkout nazwa^
+git branch -f (nazwa)
+git reset
+git revert
+git cherry-pick
+git reabse -i
+git tag
+git clone
+git fetch
+git pull
+git fakeTeamwork
+git push
